0032801a9b89d1378900b4d76adf5727083b6113
이인영 <2innnnn0@kakao.com>
2023-03-05 13:55:02 +0900

    Update GROUP_BY.sql

--- programmers_sql_test/GROUP_BY.sql
+++ programmers_sql_test/GROUP_BY.sql
@@ -1,5 +1,35 @@
 https://school.programmers.co.kr/learn/courses/30/parts/17044
 
+# 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기 https://school.programmers.co.kr/learn/courses/30/lessons/151139
+--  "(FROM)CAR_RENTAL_COMPANY_RENTAL_HISTORY 테이블에서" 
+--  "(WHERE)"대여 시작일을 기준으로 2022년 8월부터 2022년 10월까지" "총 대여 횟수가 5회 이상"인 자동차들에 대해서" 
+--  "(SELECT)해당 기간 동안의 월별 자동차 ID 별 총 대여 횟수(컬럼명: RECORDS) 리스트를 출력"하는 SQL문을 작성해주세요. 
+--  "(ORDER BY)결과는 월을 기준으로 오름차순 정렬하고, 월이 같다면 자동차 ID를 기준으로 내림차순 정렬"해주세요. 
+--  특정 월의 총 대여 횟수가 0인 경우에는 결과에서 제외해주세요
+
+-- 코드를 입력하세요
+SELECT
+    MONTH(START_DATE) AS MONTH,
+    CAR_ID,
+    COUNT(HISTORY_ID) AS RECORDS
+FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+WHERE START_DATE BETWEEN '2022-08-01' AND '2022-10-31' -- 2022년 8월부터 2022년 10월
+AND CAR_ID IN (
+                SELECT CAR_ID
+               FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+               WHERE START_DATE BETWEEN '2022-08-01' AND '2022-10-31' -- 2022년 8월부터 2022년 10월
+               GROUP BY CAR_ID
+               HAVING COUNT(HISTORY_ID) >= 5
+              ) -- 자동차별 총대여횟수 5회 이상
+GROUP BY 
+    MONTH, 
+    CAR_ID
+HAVING RECORDS > 0
+ORDER BY 
+    MONTH(START_DATE), 
+    CAR_ID DESC
+
+
 # 1. 진료과별 총 예약 횟수 출력하기
 SELECT
    MCDP_CD AS 진료과코드,
